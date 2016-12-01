bb428032931e18e663e173ddc31a382d84855928
Jigar Mistry <jigarmistry24@gmail.com>
2016-12-01 09:55:37 +0530

    Call all methods and get the response

--- soap_req.py
+++ soap_req.py
@@ -14,30 +14,33 @@ imp.filter.add('http://xml.apache.org/xml-soap')
 username = "ipl"
 password = "xg8229jf"
 base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
+
 authenticationHeader = {
     "SOAPAction" : "ActionName",
     "Authorization" : "Basic %s" % base64string
 }
 
 client = Client(url=wsdl_url,location= soap_url, plugins=[ImportDoctor(imp)],headers=authenticationHeader)
-# print (client)
 
-# print (client.wsdl.services[0].ports[0].methods.values()[0])
+# Get Part Models
 a = client.factory.create("ns2:Map")
 a.item.key = "partNumber"
 a.item.value = "341241"
 # print client.service.getPartModels(a)
 
+# Get Model Sections
 b = client.factory.create("ns2:Map")
 b.item.key = "modelId"
 b.item.value = "87048"
 # print client.service.getModelSections(b)
 
+# Search Models
 c = client.factory.create("ns2:Map")
 c.item.key = "modelNumber"
 c.item.value = "mav600"
 # print client.service.searchModels(c)
 
+# Get Model Sections Parts
 d = client.factory.create("ns2:Item")
 d.key = "modelId"
 d.value = "87048"
@@ -48,9 +51,9 @@ e.value = "525454"
 
 f = client.factory.create("ns2:Map")
 f.item = [d,e]
-
 # print client.service.getModelSectionsParts(f)
 
+# Model Search Parts
 g = client.factory.create("ns2:Item")
 g.key = "keyword"
 g.value = "motor"
